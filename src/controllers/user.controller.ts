3c907b1b3b7978368e997cdb42cccecdbd0cef33
valncabs <cabasmirandavalen@gmail.com>
2026-08-29 17:08:53 -0500

    service

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,6 +1,6 @@
-import type { Request, Response } from "express";
-import type { createUserDto } from "../dto/user.dto";
-
+import { Request, Response } from "express";
+import { CreateUserDto } from "../dto/user.dto";
+import { createUserService } from "../services/user.service";
 export const getUsers = (req: Request, res: Response):void =>{
     
     res.status(200).json({
@@ -15,21 +15,14 @@ export const getUser = (req: Request, res: Response):void=>{
     })
 }
 
-
-
 export const createUser = (req: Request, res: Response): void => {
 
-    if (!req.body.name || !req.body.email) {
-        res.status(400).json({
-            message: "Nombre y email son obligatorios"
-        });
-        return;
-    }
+    const user: CreateUserDto = req.body;
 
-    const user : createUserDto = req.body;
+    const result = createUserService(user);
 
     res.status(201).json({
         message: "Usuario creado correctamente",
-        user: user
+        user: result
     });
 };
---
+++ src/services/user.service.ts
@@ -0,0 +1,7 @@
+import { CreateUserDto } from "../dto/user.dto";
+
+
+
+export const createUserService = (user: CreateUserDto) => {
+    return user;
+};
